25e5e4ad718491d8e71fe451ef135d2c836d5cb1
Luka Gabric <luka.gabric@gmail.com>
2016-04-05 10:07:28 +0200

    Added status

--- ESP8266Light_Arduino.ino
+++ ESP8266Light_Arduino.ino
@@ -3,45 +3,56 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-const char *ssid = ".....";
-const char *password = ".....";
+const char *ssid = "....";
+const char *password = "....";
 
-int ledPin = 2;
+int relayPin = 2;
+boolean lightOn = false;
 
 ESP8266WebServer server(8081);
 
-void handleRoot() {
-  server.send(200, "text/plain", "Hello!");
+void updateRelayPin() {
+  int pinValue = lightOn ? 1 : 0;
+  digitalWrite(relayPin, pinValue);
+}
+
+void configureLightOn(boolean turnOn) {
+  lightOn = turnOn;
+  updateRelayPin();  
 }
 
 void handleLightOn() {
-  digitalWrite(ledPin, HIGH);
-  server.send(200, "text/plain", "Light is ON!");
+  configureLightOn(true);
+  server.send(200, "application/json", "{\"lightOn\":true}");
 }
 
 void handleLightOff() {
-  digitalWrite(ledPin, LOW);
-  server.send(200, "text/plain", "Light is OFF");
+  configureLightOn(false);
+  server.send(200, "application/json", "{\"lightOn\":false}");
 }
 
-void handleNotFound(){
+void handleLightStatus() {
+  String resp = String(String("{\"lightOn\":") + String(lightOn ? "true" : "false") + String("}"));
+  server.send(200, "application/json", resp);
+}
+
+void handleNotFound() {
   String message = "File Not Found\n\n";
   message += "URI: ";
   message += server.uri();
   message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
   message += "\nArguments: ";
   message += server.args();
   message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
+  for (uint8_t i = 0; i < server.args(); i++){
     message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
   server.send(404, "text/plain", message);
 }
 
-void setup(void){
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, HIGH);
+void setup() {
+  pinMode(relayPin, OUTPUT);
   Serial.begin(115200);
   WiFi.begin(ssid, password);
   Serial.println("");
@@ -61,21 +72,17 @@ void setup(void){
     Serial.println("MDNS responder started");
   }
 
-  server.on("/", handleRoot);
-
+  server.on("/light/status", handleLightStatus);
   server.on("/light/on", handleLightOn);
   server.on("/light/off", handleLightOff);
   
-  server.on("/inline", [](){
-    server.send(200, "text/plain", "Inline");
-  });
-
   server.onNotFound(handleNotFound);
 
   server.begin();
   Serial.println("HTTP server started");
 }
 
-void loop(void){
+void loop() {
   server.handleClient();
 }
+
